6dad0edf8a1f6976ef543c7d8cf7465480373d40
Johan Hanses <johanhanses@gmail.com>
2025-09-18 19:38:53 +0200

    disable inlay hints

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.opt.wrap = true
+vim.lsp.inlay_hint.enable(false)
 vim.opt.relativenumber = false
 -- vim.opt.clipboard = "unnamedplus"
 
