e4cb2475c90bf51be4514a35f464f2e8171f7f43
Johan Hanses <johanhanses@gmail.com>
2025-09-22 09:54:02 +0200

    neo-tree config

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -3,14 +3,17 @@ return {
   opts = {
     filesystem = {
       filtered_items = {
-        visible = true,
+        visible = false,
         show_hidden_count = true,
         hide_dotfiles = false,
-        hide_gitignored = true,
+        hide_gitignored = false,
         hide_by_name = {
           ".git",
           ".DS_Store",
           -- 'thumbs.db',
+          "node_modules",
+          ".vscode",
+          "build",
         },
         never_show = {},
       },
